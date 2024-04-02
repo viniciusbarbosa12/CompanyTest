6bab69db6019dff1e6597e34fdf63c0bb727aeee
Vinicius Barbosa <viniciusbarbosa.gt4w@gmail.com>
2024-04-02 20:38:40 -0300

    initializing ef core

---
+++ API/Models/entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Models.entities
+{
+    public class Category
+    {
+
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Codigo { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ API/Models/entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.entities
+{
+    public class Product
+    {
+        public Guid ProdutoId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
